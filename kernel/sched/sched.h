dfa3d044b1180ded650fa51f6a18c538c32ed9de
Jhonattan Rodriguez <jjrodriguezn@unal.edu.co>
2014-05-06 12:20:30 -0500

    Update sched.h

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -564,7 +564,7 @@ struct rq {
 	 */
 	unsigned long nr_uninterruptible;
 
-	struct task_struct *curr, *idle, *stop;
+	static extern struct task_struct *curr, *idle, *stop;
 	unsigned long next_balance;
 	struct mm_struct *prev_mm;
 
